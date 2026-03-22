d8de9d5994c64402a13169827c9ef1052c0c1271
web-demo-user <1608198496@qq.com>
2026-03-23 00:31:23 +0800

    feat: 消息模块基础功能
    - 通知列表展示
    - 会话列表展示
    - 未读消息红点
    - 登录状态检查

--- miniprogram/pages/messages/messages.js
+++ miniprogram/pages/messages/messages.js
@@ -1,82 +1,69 @@
 // pages/messages/messages.js
 const app = getApp();
-const db = wx.cloud.database();
+const api = require('../../utils/api.js');
 
 Page({
   data: {
-    messages: [],
-    loading: true,
-    isLoggedIn: false
+    isLoggedIn: false,
+    showLoginModal: false,
+    notifications: [],
+    conversations: []
   },
 
   onLoad: function () {
     this.checkLogin();
-    this.loadMessages();
   },
 
   onShow: function () {
     this.checkLogin();
+    if (this.data.isLoggedIn) {
+      this.loadData();
+    }
   },
 
+  // 检查登录状态
   checkLogin: function () {
     const isLoggedIn = app.globalData.isLoggedIn;
     this.setData({ isLoggedIn });
   },
 
-  loadMessages: function () {
+  // 加载数据
+  loadData: async function () {
     // 模拟数据
-    const mockMessages = [
-      {
-        _id: 'msg_001',
-        type: 'apply',
-        title: '新的申请',
-        content: '小王申请加入你的东灵山行程',
-        isRead: false,
-        createTime: '10分钟前'
-      },
-      {
-        _id: 'msg_002',
-        type: 'accept',
-        title: '申请已通过',
-        content: '小李同意了你的申请，可以一起去了！',
-        isRead: true,
-        createTime: '2小时前'
-      },
-      {
-        _id: 'msg_003',
-        type: 'invite',
-        title: '收到邀请',
-        content: '小张邀请你参加八达岭长城行程',
-        isRead: true,
-        createTime: '昨天'
-      },
-      {
-        _id: 'msg_004',
-        type: 'system',
-        title: '系统通知',
-        content: '欢迎来到北京去哪玩！快去发布你的第一个行程吧',
-        isRead: true,
-        createTime: '3天前'
-      }
-    ];
-
     this.setData({
-      messages: mockMessages,
-      loading: false
+      notifications: [
+        { _id: '1', type: 'apply', content: '小明申请加入你的「颐和园」行程', time: '刚刚' },
+        { _id: '2', type: 'notice', content: '你的「长城」行程即将开始，记得准时参加', time: '1小时前' }
+      ],
+      conversations: [
+        { _id: '1', name: '颐和园行程群', avatar: 'https://picsum.photos/100/100?random=1', lastMessage: '明天见！', lastTime: '10:30', unreadCount: 2 },
+        { _id: '2', name: '小红', avatar: 'https://picsum.photos/100/100?random=2', lastMessage: '好的，没问题', lastTime: '昨天', unreadCount: 0 }
+      ]
     });
   },
 
-  // 点击消息
-  onMessageTap: function (e) {
-    const msgId = e.currentTarget.dataset.id;
-    wx.showToast({ title: '查看消息详情', icon: 'none' });
-  },
-
-  // 登录
+  // 点击登录
   onLogin: function () {
-    app.wxLogin().then(userInfo => {
-      this.setData({ isLoggedIn: true });
-      this.loadMessages();
+    this.setData({ showLoginModal: true });
+  },
+
+  // 关闭登录弹窗
+  onCloseLoginModal: function () {
+    this.setData({ showLoginModal: false });
+  },
+
+  // 登录成功
+  onLoginSuccess: function () {
+    this.setData({
+      isLoggedIn: true,
+      showLoginModal: false
     });
+    this.loadData();
+  },
+
+  // 点击会话
+  onConversationTap: function (e) {
+    const id = e.currentTarget.dataset.id;
+    wx.showToast({ title: '聊天功能开发中', icon: 'none' });
   }
 });
